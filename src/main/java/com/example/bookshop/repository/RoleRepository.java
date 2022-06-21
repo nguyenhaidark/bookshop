eb0289d819ffc85e71cf0e9a1919dee13c92c193
nguyenhaidark <nguyenhaidang25072001@gmail.com>
2022-06-21 09:15:13 +0700

    role

--- src/main/java/com/example/bookshop/repository/RoleRepository.java
+++ src/main/java/com/example/bookshop/repository/RoleRepository.java
@@ -1,15 +1,11 @@
 package com.example.bookshop.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.bookshop.entity.Role;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long>{
-	@Query("select r from Role r where r.name like %:name%")
-	List<Role> searchRoleByName(String name);
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
 }
--- src/main/java/com/example/bookshop/service/impl/RoleImpl.java
+++ src/main/java/com/example/bookshop/service/impl/RoleImpl.java
@@ -1,67 +1,15 @@
 package com.example.bookshop.service.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import com.example.bookshop.entity.Role;
-import com.example.bookshop.myfunction.GetDateNow;
 import com.example.bookshop.repository.RoleRepository;
 import com.example.bookshop.service.RoleService;
 
 @Service
-public class RoleImpl implements RoleService{
-	
+public class RoleImpl implements RoleService {
+
 	@Autowired
 	RoleRepository roleRepository;
-
-	@Override
-	public List<Role> getRoles(int pageNo, int pageSize, String sortBy, String sortDir) {
-		Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
-				: Sort.by(sortBy).descending();
-		Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
-		Page<Role> role = roleRepository.findAll(pageable);
-		List<Role> roles = new ArrayList<>();
-		roles = role.getContent();
-		return roles;
-	}
-
-	@Override
-	public Role getRoleById(Long id) {
-		return roleRepository.findById(id).get();
-	}
-
-	@Override
-	public List<Role> getRoleByName(String name) {
-		return roleRepository.searchRoleByName(name);
-	}
-
-	@Override
-	public Role addRole(Role role) {
-		role.setCreatedDate(GetDateNow.getDate());
-		role.setUpdatedDate("undefined");
-		return roleRepository.save(role);
-	}
-
-	@Override
-	public Role updateRole(Role role) {
-		Role exRole = roleRepository.findById(role.getId()).orElse(null);
-		exRole.setUpdatedDate(GetDateNow.getDate());
-		exRole.setName(role.getName());
-		exRole.setStatus(role.isStatus());
-		return roleRepository.save(exRole);
-	}
-
-	@Override
-	public void deleteRole(Long id) {
-		roleRepository.deleteById(id);
-		
-	}
 
 }
